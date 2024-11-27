0f68666055f8905eea83e0e91cb000f57df04e36
EvgeniyEvgenievich <Dedjoze@gmail.com>
2024-11-27 17:40:02 +0300

    Update module_7_4.py

--- module_7_4.py
+++ module_7_4.py
@@ -1,41 +1,36 @@
-team1_num = 6
-team2_num = 6
+team1 = 'Мастера кода'
+team2 = 'Волшебники данных'
+
+
+def num(team1_num=0, team2_num=0):
+    print('В команде %s участников: %s !' % (team1, team1_num))
+    print('Итого сегодня в командах участников: %s и %s !' % (team1_num, team2_num))
+
+
+score1 = 0
+score2 = 0
+
+
+def time(team1_time=0, team2_time=0, tasks_total=0):
+    time1 = team1_time
+    time2 = team2_time
+
+    print('Команда {} решила задач: {}!'.format(team2, score2))
+    print('{} решили задачи за {} cек. !'.format(team2, time2))
+
+
+def challenge_result(tasks_total=0, time_avg=0):
+    print(f'Команды решили {score1} и {score2} задач')
+    if score1 > score2:
+        print(f'Результат битвы: победа команды {team1} !')
+    else:
+        print(f'Результат битвы: победа команды {team2} !')
+
+    print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу')
+
+
+num(team1_num=5, team2_num=6)
 score1 = 40
 score2 = 42
-team1_time = 1552.512
-team2_time = 2153.31451
-tasks_total = 82
-time_avg = 45.2
-challenge_result = 'Победа команды Волшебники данных!'
-
-if score1 > score2 or score1 == score2 and team1_time > team2_time:
-    challenge_result = 'Победа команды "Мастера кода!"'
-elif score1 < score2 or score1 == score2 and team1_time < team2_time:
-    challenge_result = 'Победа команды "Волшебники Данных!"'
-else:
-    challenge_result = 'Ничья!'
-
-#Использование '%'
-result_Masters = "В команде 'Мастера кода' участников: %.f !" % team1_num
-result_Wizards = "В команде 'Волшебники данных': %.f !" % team2_num
-result = "Итого сегодня в командах участников: %.f и %.f" % (team1_num, team2_num)
-
-#Использование format()
-result1 = "Команда 'Мастера кода' решила задач {} !".format(score1)
-result2 = "Команда 'Волшебники данныйх' решила задач {} !".format(score2)
-
-#Использование f-строк
-result3 = f"Команды решили {score1} и {score2} задач!"
-result4 = f"Результат битвы: {challenge_result}"
-result5 = f"Сегодня было решено {tasks_total} задач в среднем по {time_avg:.1f} секунд на задачу"
-
-#Запись в файл
-with open("result.txt", "w", encoding="utf-8") as f:
-    f.write(result_Masters + '\n')
-    f.write(result_Wizards + '\n')
-    f.write(result + '\n')
-    f.write(result1 + '\n')
-    f.write(result2 + '\n')
-    f.write(result3 + '\n')
-    f.write(result4 + '\n')
-    f.write(result5 + '\n')
+time(team1_time=1552.512, team2_time=2153.31451)
+challenge_result(tasks_total=82, time_avg=45.2)
